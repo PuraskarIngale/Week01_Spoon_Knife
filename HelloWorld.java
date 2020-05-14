fe17d364a60dcc38d0c00ce16830e3b9e0b4c1e9
PuraskarIngale <puraskar_theaward@yahoo.com>
2020-05-13 21:39:41 -0400

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World - Puraskar Ingale is learning Software Versioning for a Functional programming in Analytics Class 560.51 at HUST");
+	}
+}
